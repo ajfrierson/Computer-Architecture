e0f04334c98a88c74786b3e7eff2dbb825cec5eb
Alvin <alvin.frierson@fuller.edu>
2019-06-10 17:25:53 -0700

    Get print8.ls8 running

--- ls8/cpu.h
+++ ls8/cpu.h
@@ -5,16 +5,29 @@
 struct cpu {
   // TODO
   // PC
+  unsigned char pc;
+  unsigned char fl;
   // registers (array)
+  unsigned char registers[8];
   // ram (array)
+  unsigned char ram[256];
 };
 
 // ALU operations
-enum alu_op {
-	ALU_MUL
-	// Add more here
+enum alu_op
+{
+  ALU_MUL,
+  // Add more here
+  ALU_ADD,
+  ALU_CMP,
+  ALU_AND,
+  ALU_OR,
+  ALU_XOR,
+  ALU_NOT,
+  ALU_SHL,
+  ALU_SHR,
+  ALU_MOD
 };
-
 // Instructions
 
 // These use binary literals. If these aren't available with your compiler, hex
@@ -22,11 +35,28 @@ enum alu_op {
 
 #define LDI  0b10000010
 #define PRN  0b01000111
+#define HLT  0b00000001
 // TODO: more instructions here. These can be used in cpu_run().
-
+#define MUL 0b10100010
+#define PUSH 0b01000101
+#define POP 0b01000110
+#define CALL 0b01010000
+#define RET 0b00010001
+#define ADD 0b10100000
+#define CMP 0b10100111
+#define JMP 0b01010100
+#define JEQ 0b01010101
+#define JNE 0b01010110
+#define AND 0b10101000
+#define OR 0b10101010
+#define XOR 0b10101011
+#define NOT 0b01101001
+#define SHL 0b10101100
+#define SHR 0b10101101
+#define MOD 0b10100100
 // Function declarations
 
-extern void cpu_load(struct cpu *cpu);
+extern void cpu_load(struct cpu *cpu, int argc, char *file);
 extern void cpu_init(struct cpu *cpu);
 extern void cpu_run(struct cpu *cpu);
 
